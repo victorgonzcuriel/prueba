dac0265c96cab35a3377036b8872656f3e56caaf
= <=>
2019-11-10 16:19:12 +0100

    Arreglos del finde

--- src/app/home/saludo-ref/saludo-ref.component.ts
+++ src/app/home/saludo-ref/saludo-ref.component.ts
@@ -7,12 +7,13 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class SaludoRefComponent implements OnInit {
 
+  msg: string;
   // Decorador para ver cosas del padre al hijo
   @ViewChild('refNombre', {static: true}) nodoNombre: ElementRef;
-
   constructor() { }
 
   ngOnInit() {
+    this.msg = 'Aquí va tu apellido';
     console.log(this.nodoNombre.nativeElement);
     console.dir(this.nodoNombre);
   }
