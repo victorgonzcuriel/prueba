aefff8b06e2373ea3e6e4fd26a6d96d394359caf
= <=>
2019-11-16 14:14:04 +0100

    arreglos de la semana

--- src/app/utils/destacar.directive.ts
+++ src/app/utils/destacar.directive.ts
@@ -11,17 +11,16 @@ export class DestacarDirective implements OnInit {
 
   // Con elemento tengo el elemento del DOM donde se le llama
   constructor(public element: ElementRef) {
-    console.log('Directiva Instanciada');
-    // Añadir una clase css
-    this.element.nativeElement.classList.add('destacar');
-    this.element.nativeElement.innerHTML += this.docDestacar;
-    console.dir(this.element);
   }
 
   ngOnInit(): void {
     console.log('Directiva aplicada');
     console.dir(this.element);
     console.log(this.docDestacar);
+    // Añadir una clase css
+    this.element.nativeElement.classList.add('destacar');
+    this.element.nativeElement.innerHTML += this.docDestacar;
+    console.dir(this.element);
   }
 
   // Ejemplo de manejador de eventos (en este caso click)
